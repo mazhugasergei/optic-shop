ae5bb3ca33dcf37b2c6ee1ebf7937e399721df69
Markus Wedler <markuswedler8@gmail.com>
2023-05-31 20:16:08 +1000

    Refactor code (main section)

--- src/App.js
+++ src/App.js
@@ -5,13 +5,8 @@ import BG from "./components/BG"
 import Header from "./components/Header"
 import NavRight from "./components/NavRight"
 import NavLeft from "./components/NavLeft"
+import Main from "./components/Main"
 import CTA from "./components/CTA"
-// images
-import blackGlasses from "./assets/images/glasses/black.webp"
-import blueGlasses from "./assets/images/glasses/blue.webp"
-import redGlasses from "./assets/images/glasses/red.webp"
-import greenGlasses from "./assets/images/glasses/green.webp"
-import yellowGlasses from "./assets/images/glasses/yellow.webp"
 
 const App = () => {
   const transition = 700
@@ -107,44 +102,6 @@ const App = () => {
     setCurrentSlide(currentSlide + 1)
   }
 
-  const onLoad = () => {
-    // set glasses text event listener
-    window.addEventListener("mousemove", (e) => {
-      document.querySelectorAll('.glasses-cont .text').forEach(text => {
-        text.style.transition = transition/1000 + "s linear"
-        text.style.transform = "translate(calc(" +
-          " -0.03 * (" + e.clientX + "px - 50vw) " // X
-          + "), calc(" +
-          " -0.03 * (" + e.clientY + "px - 50vh) " // Y
-          + "))"
-      })
-    })
-    // set glasses images style
-    document.querySelectorAll('.glasses-cont .images img').forEach(glasses => {
-      glasses.style.transition = transition/2000 + "s"
-      glasses.style.transitionDelay = transition/6000 + "s"
-    })
-    // set lenses style
-    document.querySelectorAll('.lense').forEach(lense => {
-      lense.style.transition = transition/3000 + "s"
-      lense.style.transitionDelay = transition/5000 + "s"
-    })
-    // set glasses-cont style
-    document.querySelector('.glasses-cont').style.transition = transition/2000 + "s"
-    document.querySelector('.glasses-cont').style.transitionDelay = transition/5000 + "s" 
-    document.querySelector('.glasses-cont').style.transform = "translateY(0)"
-    document.querySelector('.glasses-cont').style.opacity = 1
-  }
-
-  useEffect(()=>{
-    // onLoad
-    if(document.readyState === "complete") onLoad()
-    else{
-      window.addEventListener("load", onLoad)
-      return () => window.removeEventListener("load", onLoad)
-    }
-  }, [])
-
   const onSlideChange = () => {
     // recalculate currentSlide
     if(currentSlide < 0) setCurrentSlide(4)
@@ -202,35 +159,7 @@ const App = () => {
 
       <Header transition={transition} />
 
-      <section className="main">
-        <div className="headline-cont">
-          <div className="text headline"><span></span></div>
-        </div>
-        <div className="glasses-cont" aria-hidden="true">
-          <div className="text glasses"><span></span></div>
-          <div className="lenses">
-            <div className="lense">
-              <div className="text glasses"><span></span></div>
-            </div>
-            <div className="lense">
-              <div className="text glasses"><span></span></div>
-            </div>
-          </div>
-          <div className="images">
-            <img src={blackGlasses}/>
-            <img src={blueGlasses}/>
-            <img src={redGlasses}/>
-            <img src={greenGlasses}/>
-            <img src={yellowGlasses}/>
-          </div>
-        </div>
-        <div className="num-cont">
-          <div className="text num"><span></span></div>
-        </div>
-        <div className="paragraph-cont">
-          <div className="text paragraph"><span></span></div>
-        </div>
-      </section>
+      <Main {...{transition}} />
 
       <NavLeft {...{transition, slide, colors, currentSlide, setSlideDirection, setCurrentSlide}} />
 
