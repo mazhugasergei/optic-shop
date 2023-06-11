52255c9f140e4dee7ee27c2d302eb1a1935cbb13
Markus Wedler <markuswedler8@gmail.com>
2023-06-11 23:36:42 +1000

    Revert "Refactor code"
    This reverts commit 8d7fefa7bbc56b1c327b4863458f660b182be982.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './assets/style/index.css'
+import './style/index.css'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
